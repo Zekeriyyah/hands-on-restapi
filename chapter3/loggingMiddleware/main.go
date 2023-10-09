0ed50ddc49abba3c558f29580788eefb6a1c6254
go-dev <awwalcodestar@gmail.com>
2023-10-09 18:20:30 +0100

    Feat: Implementing handlers middleware to perform api-wide logging

---
+++ chapter3/loggingMiddleware/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	log.Println("Processing Request....")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+	log.Println("Finished processing request....")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handle)
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	http.ListenAndServe(":8000", loggedRouter)
+}
